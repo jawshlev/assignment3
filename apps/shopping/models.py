bbd8bc86aab334446a2f3398784cfaf0d2442083
josh lev <soybr3ad@gmail.com>
2024-10-29 19:01:49 -0700

    Adding a table to hold values for my list
    Used to fill the required information for my table

--- apps/shopping/models.py
+++ apps/shopping/models.py
@@ -14,8 +14,12 @@ def get_time():
     return datetime.datetime.utcnow()
 
 # Add here any table definition you need. Below is an example.
-# db.define_table('shopping_list',
-#    Field('product_name', requires=IS_NOT_EMPTY()),
-#    )
-
+db.define_table('shopping_list',
+     Field('product_name', requires=IS_NOT_EMPTY()),
+     Field('purchased', 'boolean', default=False),
+     Field('added_by', default=get_user_email),
+     Field('added_on', default=get_time),
+    )
+#requires makes it so it's required
+# purpose of models.py is to create our database and our table
 db.commit()
